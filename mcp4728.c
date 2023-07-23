5dce68de2f5bc4b8c71f1fd2b81ac2b7f4369e79
Andrea Collamati <andrea.collamati@gmail.com>
2023-07-23 05:12:52 +0200

    ..

--- mcp4728.c
+++ mcp4728.c
@@ -2,9 +2,9 @@
 /*
  * Support for Microchip MCP4728
  *
- * Copyright (C) 2012 Andrea Collamati <andrea.collamati@gmail.com>
+ * Copyright (C) 2023 Andrea Collamati <andrea.collamati@gmail.com>
  *
- * Based on mcp4728 by Peter Meerwald <pmeerw@pmeerw.net>
+ * Based on mcp4725 by Peter Meerwald <pmeerw@pmeerw.net>
  *
  * Driver for the Microchip I2C 12-bit digital-to-analog quad channels
  * converter (DAC).
@@ -13,41 +13,39 @@
  * hardware)
  */
 
-#include <linux/module.h>
-#include <linux/i2c.h>
-#include <linux/err.h>
+#include <linux/bitfield.h>
 #include <linux/delay.h>
-#include <linux/regulator/consumer.h>
-#include <linux/mod_devicetable.h>
-#include <linux/property.h>
-
+#include <linux/err.h>
+#include <linux/i2c.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+#include <linux/module.h>
+#include <linux/mod_devicetable.h>
+#include <linux/property.h>
+#include <linux/regulator/consumer.h>
 
-#define MCP4728_DRV_NAME "mcp4728"
 
 #define MCP4728_RESOLUTION 12
 #define MCP4728_N_CHANNELS 4
 
-#define MCP4728_CMD_POS 3
-#define MCP4728_CMD_UDAC_POS 0
-#define MCP4728_CMD_CH_SEL_POS 1
+#define MCP4728_CMD_FIELD	GENMASK(7, 3)
+#define MCP4728_CHSEL_FIELD	GENMASK(2, 1)
+#define MCP4728_UDAC_FIELD	BIT(0)
 
-#define MCP4728_CMD_VREF_MASK 0x80
-#define MCP4728_CMD_VREF_POS 7
+#define MCP4728_VREF_FIELD	BIT(7)
+#define MCP4728_PDMODE_FIELD	GENMASK(6,5)
+#define MCP4728_GAIN_FIELD	BIT(4)
 
-#define MCP4728_CMD_PDMODE_MASK 0x60
-#define MCP4728_CMD_PDMODE_POS 5
+#define MCP4728_DAC_H_FIELD	GENMASK(3, 0)
+#define MCP4728_DAC_L_FIELD	GENMASK(7, 0)
 
-#define MCP4728_CMD_GAIN_MASK 0x10
-#define MCP4728_CMD_GAIN_POS 4
+#define MCP4728_RDY_FIELD	BIT(7)
 
-#define MCP4728_MW_CMD 0x08 // Multiwrite Command
-#define MCP4728_SW_CMD 0x0A // Sequential Write Command (include eeprom)
+#define MCP4728_MW_CMD		0x08 /* Multiwrite Command */
+#define MCP4728_SW_CMD		0x0A /* Sequential Write Command with EEPROM */
 
-#define MCP4728_READ_RESPONSE_LEN (MCP4728_N_CHANNELS * 3 * 2) // Read Message
-#define MCP4728_WRITE_EEPROM_LEN \
-	(1 + MCP4728_N_CHANNELS * 2) // Sequential Write
+#define MCP4728_READ_RESPONSE_LEN (MCP4728_N_CHANNELS * 3 * 2)
+#define MCP4728_WRITE_EEPROM_LEN (1 + MCP4728_N_CHANNELS * 2)
 
 enum vref_mode {
 	MCP4728_VREF_EXTERNAL_VDD = 0,
@@ -74,19 +72,20 @@ struct mcp4728_channel_data {
 
 struct mcp4728_data {
 	struct i2c_client *client;
-	int id;
 	struct regulator *vdd_reg;
 	bool powerdown;
 	struct mcp4728_channel_data channel_data[MCP4728_N_CHANNELS];
 };
 
-#define MCP4728_CHAN(chan)                                                     \
-	{                                                                      \
-		.type = IIO_VOLTAGE, .output = 1, .indexed = 1,                \
-		.channel = chan, .info_mask_separate = BIT(IIO_CHAN_INFO_RAW), \
-		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),          \
-		.ext_info = mcp4728_ext_info,                                  \
-	}
+#define MCP4728_CHAN(chan) {					\
+	.type = IIO_VOLTAGE,					\
+	.output = 1,						\
+	.indexed = 1,						\
+	.channel = chan,					\
+	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),		\
+	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),	\
+	.ext_info = mcp4728_ext_info,				\
+}
 
 static int mcp4728_suspend(struct device *dev);
 static int mcp4728_resume(struct device *dev);
@@ -102,6 +101,7 @@ static ssize_t mcp4728_store_eeprom(struct device *dev,
 	u8 inbuf[3];
 	bool state;
 	int ret;
+	unsigned int i;
 
 	ret = kstrtobool(buf, &state);
 	if (ret < 0)
@@ -110,22 +110,23 @@ static ssize_t mcp4728_store_eeprom(struct device *dev,
 	if (!state)
 		return 0;
 
-	outbuf[0] = MCP4728_SW_CMD << MCP4728_CMD_POS; // Command ID
+	outbuf[0] = FIELD_PREP(MCP4728_CMD_FIELD, MCP4728_SW_CMD);
 
-	for (int i = 0; i < MCP4728_N_CHANNELS; i++) {
-		struct mcp4728_channel_data *ch = &(data->channel_data[i]);
+	for (i = 0; i < MCP4728_N_CHANNELS; i++) {
+		struct mcp4728_channel_data *ch = &data->channel_data[i];
 		int offset = 1 + i * 2;
 
-		outbuf[offset] = ch->ref_mode << MCP4728_CMD_VREF_POS;
+		outbuf[offset] = FIELD_PREP(MCP4728_VREF_FIELD, ch->ref_mode);
+
 		if (data->powerdown) {
 			u8 mcp4728_pd_mode = ch->pd_mode + 1;
 
-			outbuf[1] |= mcp4728_pd_mode << MCP4728_CMD_PDMODE_POS;
+			outbuf[offset] |= FIELD_PREP(MCP4728_PDMODE_FIELD, mcp4728_pd_mode);
 		}
 
-		outbuf[offset] |= ch->g_mode << MCP4728_CMD_GAIN_POS;
-		outbuf[offset] |= ch->dac_value >> 8;
-		outbuf[offset + 1] = ch->dac_value & 0xff;
+		outbuf[offset] |= FIELD_PREP(MCP4728_GAIN_FIELD, ch->g_mode);
+		outbuf[offset] |= FIELD_PREP(MCP4728_DAC_H_FIELD, ch->dac_value >> 8);
+		outbuf[offset + 1] = FIELD_PREP(MCP4728_DAC_L_FIELD, ch->dac_value);
 	}
 
 	ret = i2c_master_send(data->client, outbuf, MCP4728_WRITE_EEPROM_LEN);
@@ -143,7 +144,7 @@ static ssize_t mcp4728_store_eeprom(struct device *dev,
 		else if (ret != 3)
 			return -EIO;
 
-		if (inbuf[0] & 0x80) // check RDY flag
+		if (FIELD_GET(MCP4728_RDY_FIELD, inbuf[0]))
 			break;
 	}
 
@@ -155,7 +156,7 @@ static ssize_t mcp4728_store_eeprom(struct device *dev,
 	return len;
 }
 
-static IIO_DEVICE_ATTR(store_eeprom, S_IWUSR, NULL, mcp4728_store_eeprom, 0);
+static IIO_DEVICE_ATTR(store_eeprom, 0200, NULL, mcp4728_store_eeprom, 0);
 
 static struct attribute *mcp4728_attributes[] = {
 	&iio_dev_attr_store_eeprom.dev_attr.attr,
@@ -166,48 +167,41 @@ static const struct attribute_group mcp4728_attribute_group = {
 	.attrs = mcp4728_attributes,
 };
 
-enum chip_id {
-	MCP4728,
-};
-
 static int mcp4728_program_channel_cfg(int channel, struct iio_dev *indio_dev)
 {
 	struct mcp4728_data *data = iio_priv(indio_dev);
-	struct mcp4728_channel_data *ch = &(data->channel_data[channel]);
+	struct mcp4728_channel_data *ch = &data->channel_data[channel];
 	u8 outbuf[3];
 	int ret;
 
-	outbuf[0] = MCP4728_MW_CMD << MCP4728_CMD_POS; // Command ID
-	outbuf[0] |= channel << MCP4728_CMD_CH_SEL_POS; // Channel Selector
-	outbuf[0] |= 0; // UDAC = 0
+	outbuf[0] = FIELD_PREP(MCP4728_CMD_FIELD, MCP4728_MW_CMD);
+	outbuf[0] |= FIELD_PREP(MCP4728_CHSEL_FIELD, channel);
+	outbuf[0] |= FIELD_PREP(MCP4728_UDAC_FIELD, 0);
 
-	outbuf[1] = ch->ref_mode << MCP4728_CMD_VREF_POS;
+	outbuf[1] = FIELD_PREP(MCP4728_VREF_FIELD, ch->ref_mode);
+
 	if (data->powerdown) {
 		u8 mcp4728_pd_mode = ch->pd_mode + 1;
 
-		outbuf[1] |= mcp4728_pd_mode << MCP4728_CMD_PDMODE_POS;
+		outbuf[1] |= FIELD_PREP(MCP4728_PDMODE_FIELD, mcp4728_pd_mode);
 	}
 
-	outbuf[1] |= ch->g_mode << MCP4728_CMD_GAIN_POS;
-
-	outbuf[1] |= ch->dac_value >> 8;
-	outbuf[2] = ch->dac_value & 0xff;
+	outbuf[1] |= FIELD_PREP(MCP4728_GAIN_FIELD, ch->g_mode);
+	outbuf[1] |= FIELD_PREP(MCP4728_DAC_H_FIELD, ch->dac_value >> 8);
+	outbuf[2] = FIELD_PREP(MCP4728_DAC_L_FIELD, ch->dac_value);
 
 	ret = i2c_master_send(data->client, outbuf, 3);
 	if (ret < 0)
 		return ret;
 	else if (ret != 3)
 		return -EIO;
-	else
-		return 0;
+
+	return 0;
 }
 
-// powerdown mode
-static const char *const mcp4728_powerdown_modes[] = {
-	"1kohm_to_gnd",
-	"100kohm_to_gnd",
-	"500kohm_to_gnd"
-};
+static const char *const mcp4728_powerdown_modes[] = { "1kohm_to_gnd",
+						       "100kohm_to_gnd",
+						       "500kohm_to_gnd" };
 
 static int mcp4728_get_powerdown_mode(struct iio_dev *indio_dev,
 				      const struct iio_chan_spec *chan)
@@ -255,6 +249,7 @@ static ssize_t mcp4728_write_powerdown(struct iio_dev *indio_dev,
 		ret = mcp4728_suspend(&data->client->dev);
 	else
 		ret = mcp4728_resume(&data->client->dev);
+
 	if (ret < 0)
 		return ret;
 
@@ -268,7 +263,6 @@ static const struct iio_enum mcp4728_powerdown_mode_enum = {
 	.set = mcp4728_set_powerdown_mode,
 };
 
-// vref mode
 static const char *const mcp4728_vref_modes[] = {
 	"vdd_ext",
 	"internal",
@@ -287,23 +281,20 @@ static int mcp4728_set_vref_mode(struct iio_dev *indio_dev,
 				 unsigned int mode)
 {
 	struct mcp4728_data *data = iio_priv(indio_dev);
-	int ret;
 
 	data->channel_data[chan->channel].ref_mode = mode;
 
 	if (mode == MCP4728_VREF_EXTERNAL_VDD &&
 	    data->channel_data[chan->channel].g_mode == MCP4728_GAIN_X2) {
-		dev_warn(
-			&data->client->dev,
-			"CH%d: Gain x2 not effective when vref is vdd, force to x1",
-			chan->channel);
+		dev_warn(&data->client->dev,
+			 "CH%d: Gain x2 not effective when vref is vdd, force to x1",
+			 chan->channel);
 		data->channel_data[chan->channel].g_mode = MCP4728_GAIN_X1;
 	}
 
-	ret = mcp4728_program_channel_cfg(chan->channel, indio_dev);
-
-	return ret;
+	return mcp4728_program_channel_cfg(chan->channel, indio_dev);
 }
+
 static const struct iio_enum mcp4728_vref_mode_enum = {
 	.items = mcp4728_vref_modes,
 	.num_items = ARRAY_SIZE(mcp4728_vref_modes),
@@ -311,7 +302,6 @@ static const struct iio_enum mcp4728_vref_mode_enum = {
 	.set = mcp4728_set_vref_mode,
 };
 
-// gain
 static const char *const mcp4728_gain_modes[] = {
 	"x1",
 	"x2",
@@ -471,37 +461,39 @@ static const struct iio_info mcp4728_info = {
 
 static int mcp4728_suspend(struct device *dev)
 {
-	struct iio_dev *indio_dev = i2c_get_clientdata(to_i2c_client(dev));
+	struct iio_dev *indio_dev = dev_get_drvdata(dev);
 	struct mcp4728_data *data = iio_priv(indio_dev);
-	int err = 0;
+	unsigned int i;
 
 	data->powerdown = true;
 
-	for (int i = 0; i < MCP4728_N_CHANNELS; i++) {
-		int ret = mcp4728_program_channel_cfg(i, indio_dev);
+	for (i = 0; i < MCP4728_N_CHANNELS; i++) {
+		int err = mcp4728_program_channel_cfg(i, indio_dev);
 
-		if (ret)
-			err = ret; //save last error
+		if (err)
+			return err;
 	}
-	return err;
+	return 0;
 }
 
 static int mcp4728_resume(struct device *dev)
 {
-	struct iio_dev *indio_dev = i2c_get_clientdata(to_i2c_client(dev));
+	struct iio_dev *indio_dev = dev_get_drvdata(dev);
 	struct mcp4728_data *data = iio_priv(indio_dev);
 	int err = 0;
+	unsigned int i;
 
 	data->powerdown = false;
 
-	for (int i = 0; i < MCP4728_N_CHANNELS; i++) {
+	for (i = 0; i < MCP4728_N_CHANNELS; i++) {
 		int ret = mcp4728_program_channel_cfg(i, indio_dev);
 
 		if (ret)
-			err = ret; //save last error
+			err = ret;
 	}
 	return err;
 }
+
 static DEFINE_SIMPLE_DEV_PM_OPS(mcp4728_pm_ops, mcp4728_suspend,
 				mcp4728_resume);
 
@@ -509,64 +501,64 @@ static int mcp4728_init_channels_data(struct mcp4728_data *data)
 {
 	u8 inbuf[MCP4728_READ_RESPONSE_LEN];
 	int ret;
+	unsigned int i;
 
 	ret = i2c_master_recv(data->client, inbuf, MCP4728_READ_RESPONSE_LEN);
 	if (ret < 0) {
 		dev_err(&data->client->dev,
-			"failed to read mcp5748 conf. Err=%d\n", ret);
+			"failed to read mcp4728 conf. Err=%d\n", ret);
 		return ret;
 	} else if (ret != MCP4728_READ_RESPONSE_LEN) {
 		dev_err(&data->client->dev,
-			"failed to read mcp5748 conf. Wrong Response Len ret=%d\n",
+			"failed to read mcp4728 conf. Wrong Response Len ret=%d\n",
 			ret);
 		return -EIO;
 	}
 
-	for (int i = 0; i < MCP4728_N_CHANNELS; i++) {
-		struct mcp4728_channel_data *ch = &(data->channel_data[i]);
+	for (i = 0; i < MCP4728_N_CHANNELS; i++) {
+		struct mcp4728_channel_data *ch = &data->channel_data[i];
 		u8 r2 = inbuf[i * 6 + 1];
 		u8 r3 = inbuf[i * 6 + 2];
 		u32 dac_mv;
 
-		ch->dac_value = (r2 & 0x0F) << 8 | r3;
-		ch->ref_mode = (r2 & MCP4728_CMD_VREF_MASK) >>
-			       MCP4728_CMD_VREF_POS;
-		ch->pd_mode = (r2 & MCP4728_CMD_PDMODE_MASK) >>
-			      MCP4728_CMD_PDMODE_POS;
-		ch->g_mode = (r2 & MCP4728_CMD_GAIN_MASK) >>
-			     MCP4728_CMD_GAIN_POS;
-		if (ch->g_mode == MCP4728_GAIN_X2 &&
-		    ch->ref_mode == MCP4728_VREF_EXTERNAL_VDD)
+		ch->dac_value = FIELD_GET(MCP4728_DAC_H_FIELD, r2) << 8 | FIELD_GET(MCP4728_DAC_L_FIELD, r3);
+		ch->ref_mode  = FIELD_GET(MCP4728_VREF_FIELD, r2);
+		ch->pd_mode   = FIELD_GET(MCP4728_PDMODE_FIELD, r2);
+		ch->g_mode    = FIELD_GET(MCP4728_GAIN_FIELD, r2);
+
+		if (ch->g_mode == MCP4728_GAIN_X2 && ch->ref_mode == MCP4728_VREF_EXTERNAL_VDD)
 			dev_warn(&data->client->dev,
 				 "CH%d: Gain x2 not effective when vref is vdd",
 				 i);
 
 		dac_mv = mcp4728_raw_to_mV(ch->dac_value, i, data);
-		dev_info(&data->client->dev,
-			 "CH%d: Voltage=%dmV VRef=%d PowerDown=%d Gain=%d\n", i,
-			 dac_mv, ch->ref_mode, ch->pd_mode, ch->g_mode);
+		dev_dbg(&data->client->dev,
+			"CH%d: Voltage=%dmV VRef=%d PowerDown=%d Gain=%d\n", i,
+			dac_mv, ch->ref_mode, ch->pd_mode, ch->g_mode);
 	}
 
 	return 0;
 }
 
-static int mcp4728_probe(struct i2c_client *client,
-			 const struct i2c_device_id *id)
+static void mcp4728_reg_disable(void *reg)
 {
+	regulator_disable(reg);
+}
+
+static int mcp4728_probe(struct i2c_client *client)
+{
+	const struct i2c_device_id *id = i2c_client_get_device_id(client);
 	struct mcp4728_data *data;
 	struct iio_dev *indio_dev;
 	int err;
 
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*data));
-	if (indio_dev == NULL)
+	if (!indio_dev)
 		return -ENOMEM;
+
 	data = iio_priv(indio_dev);
 	i2c_set_clientdata(client, indio_dev);
 	data->client = client;
-	if (dev_fwnode(&client->dev))
-		data->id = (uintptr_t)device_get_match_data(&client->dev);
-	else
-		data->id = id->driver_data;
 
 	data->vdd_reg = devm_regulator_get(&client->dev, "vdd");
 	if (IS_ERR(data->vdd_reg))
@@ -574,13 +566,17 @@ static int mcp4728_probe(struct i2c_client *client,
 
 	err = regulator_enable(data->vdd_reg);
 	if (err)
-		goto err_disable_vdd_reg;
+		return err;
+
+	err = devm_add_action_or_reset(&client->dev, mcp4728_reg_disable, data->vdd_reg);
+	if (err)
+		return err;
 
 	err = mcp4728_init_channels_data(data);
 	if (err) {
 		dev_err(&client->dev,
-			"failed to read mcp5748 current configuration\n");
-		goto err_disable_vdd_reg;
+			"failed to read mcp4728 current configuration\n");
+		return err;
 	}
 
 	indio_dev->name = id->name;
@@ -589,48 +585,32 @@ static int mcp4728_probe(struct i2c_client *client,
 	indio_dev->num_channels = MCP4728_N_CHANNELS;
 	indio_dev->modes = INDIO_DIRECT_MODE;
 
-	err = iio_device_register(indio_dev);
-	if (err)
-		goto err_disable_vdd_reg;
-
-	return 0;
-
-err_disable_vdd_reg:
-	regulator_disable(data->vdd_reg);
-
-	return err;
+	return iio_device_register(indio_dev);
 }
 
-static void mcp4728_remove(struct i2c_client *client)
-{
-	struct iio_dev *indio_dev = i2c_get_clientdata(client);
-	struct mcp4728_data *data = iio_priv(indio_dev);
-
-	iio_device_unregister(indio_dev);
-	regulator_disable(data->vdd_reg);
-}
-
-static const struct i2c_device_id mcp4728_id[] = { { "mcp4728", MCP4728 }, {} };
+static const struct i2c_device_id mcp4728_id[] = {
+	{ "mcp4728", 0 },
+	{}
+};
 MODULE_DEVICE_TABLE(i2c, mcp4728_id);
 
 static const struct of_device_id mcp4728_of_match[] = {
-	{ .compatible = "microchip,mcp4728", .data = (void *)MCP4728 },
+	{ .compatible = "microchip,mcp4728" },
 	{}
 };
 MODULE_DEVICE_TABLE(of, mcp4728_of_match);
 
 static struct i2c_driver mcp4728_driver = {
-		.driver = {
-				.name = MCP4728_DRV_NAME,
-				.of_match_table = mcp4728_of_match,
-				.pm = pm_sleep_ptr(&mcp4728_pm_ops),
-		},
-		.probe = mcp4728_probe,
-		.remove = mcp4728_remove,
-		.id_table = mcp4728_id,
+	.driver = {
+		.name = "mcp4728",
+		.of_match_table = mcp4728_of_match,
+		.pm = pm_sleep_ptr(&mcp4728_pm_ops),
+	},
+	.probe = mcp4728_probe,
+	.id_table = mcp4728_id,
 };
 module_i2c_driver(mcp4728_driver);
 
-MODULE_AUTHOR("Andrea Collamati <andrea@pmeerw.net>");
+MODULE_AUTHOR("Andrea Collamati <andrea.collamati@gmail.com>");
 MODULE_DESCRIPTION("MCP4728 12-bit DAC");
 MODULE_LICENSE("GPL");
